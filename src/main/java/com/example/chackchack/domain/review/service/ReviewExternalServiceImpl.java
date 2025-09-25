21e40d53a06cd9a3b64e60540b53d8e0b25947a9
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-25 14:24:44 +0900

    feat: Review Service 어노테이션
    Related-to: #10

--- src/main/java/com/example/chackchack/domain/review/service/ReviewExternalServiceImpl.java
+++ src/main/java/com/example/chackchack/domain/review/service/ReviewExternalServiceImpl.java
@@ -1,11 +1,8 @@
 package com.example.chackchack.domain.review.service;
 
-import com.example.chackchack.domain.review.entity.Review;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReviewExternalServiceImpl implements ReviewExternalService {
 
-    @Override
-    public Review findReviewOrElseThrow(Long reviewId) {
-        return null;
-    }
 }
