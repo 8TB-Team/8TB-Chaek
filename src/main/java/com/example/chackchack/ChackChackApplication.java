92ddd9bca6b3a605d18d75f2c9d662de5c2d5a85
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-25 11:09:31 +0900

    project init

---
+++ src/main/java/com/example/chackchack/ChackChackApplication.java
@@ -0,0 +1,13 @@
+package com.example.chackchack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChackChackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChackChackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chackchack/ChackChackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chackchack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChackChackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
