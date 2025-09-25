3369d583f3d8990b5cdcfaff86638b2b2c80e1b0
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-25 14:17:19 +0900

    feat: Review Controller 메서드
    Related-to: #10

--- src/main/java/com/example/chackchack/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/chackchack/domain/review/controller/ReviewController.java
@@ -1,4 +1,56 @@
 package com.example.chackchack.domain.review.controller;
 
+import com.example.chackchack.common.dto.response.ApiResponse;
+import com.example.chackchack.domain.review.dto.request.ReviewCreateRequest;
+import com.example.chackchack.domain.review.dto.request.ReviewUpdateRequest;
+import com.example.chackchack.domain.review.dto.response.ReviewCreateResponse;
+import com.example.chackchack.domain.review.dto.response.ReviewDetailResponse;
+import com.example.chackchack.domain.review.dto.response.ReviewUpdateResponse;
+import com.example.chackchack.domain.review.service.ReviewInternalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/book")
 public class ReviewController {
+
+    private final ReviewInternalService reviewInternalService;
+
+    @PostMapping("/{bookId}/reviews")
+    public ResponseEntity<ApiResponse<ReviewCreateResponse>> createReview(@PathVariable Long bookId,
+                                                                          @RequestBody ReviewCreateRequest request) {
+
+        return null;
+    }
+
+    @PatchMapping("/{bookId}/reviews/{reviewId}")
+    public ResponseEntity<ApiResponse<ReviewUpdateResponse>> updateReview(@PathVariable Long bookId,
+                                                                          @PathVariable Long reviewId,
+                                                                          @RequestBody ReviewUpdateRequest request) {
+
+        return null;
+    }
+
+    @DeleteMapping("/{bookId}/reviews/{reviewId}")
+    public ResponseEntity<ApiResponse<?>> deleteReview(@PathVariable Long bookId,
+                                                       @PathVariable Long reviewId) {
+
+        return null;
+    }
+
+    @GetMapping("{bookId}/reviews")
+    public ResponseEntity<ApiResponse<List<ReviewDetailResponse>>> getReviews(@PathVariable Long bookId) {
+
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<ReviewDetailResponse>>> getMyReviews() {
+
+        return null;
+    }
 }
