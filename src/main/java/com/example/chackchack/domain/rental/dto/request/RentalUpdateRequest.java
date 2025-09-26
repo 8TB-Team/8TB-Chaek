065b9b007cf843c5018f0f3a79889f44af73b4dc
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-09-26 21:14:32 +0900

    feat: 필요 없는 파일 삭제

--- src/main/java/com/example/chackchack/domain/rental/dto/request/RentalUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.chackchack.domain.rental.dto.request;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class RentalUpdateRequest {
-}
--- src/main/java/com/example/chackchack/domain/rental/dto/response/RentalResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.chackchack.domain.rental.dto.response;
-
-import lombok.Getter;
-
-@Getter
-public class RentalResponse {
-}
