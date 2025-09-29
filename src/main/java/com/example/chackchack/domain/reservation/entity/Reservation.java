b269d8697cc2c7086d544c87afc1f070b09f301b
LeeDongJae-KR <movementj98@gmail.com>
2025-09-29 15:39:47 +0900

    feat: Reservation 상태 변경 메서드 추가

--- src/main/java/com/example/chackchack/domain/reservation/entity/Reservation.java
+++ src/main/java/com/example/chackchack/domain/reservation/entity/Reservation.java
@@ -1,6 +1,8 @@
 package com.example.chackchack.domain.reservation.entity;
 
 import com.example.chackchack.common.entity.BaseEntity;
+import com.example.chackchack.domain.bookItem.entity.BookItem;
+import com.example.chackchack.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,13 +20,13 @@ public class Reservation extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id", nullable = false)
-//    private User user;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "book_id", nullable = false)
-//    private Boobk book;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_item_id", nullable = false)
+    private BookItem bookItem;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
@@ -36,5 +38,41 @@ public class Reservation extends BaseEntity {
     @Column(name = "rent_timeout")
     private LocalDateTime rentTimeout;
 
+    private Reservation(User user, BookItem bookItem, ReservationStatus reservationStatus,String priority, LocalDateTime rentTimeout) {
+        this.user = user;
+        this.bookItem = bookItem;
+        this.reservationStatus = reservationStatus;
+        this.priority = priority;
+        this.rentTimeout = rentTimeout;
 
+    }
+
+    public static Reservation of (User user, BookItem bookItem, String priority) {
+        return new Reservation(
+                user,
+                bookItem,
+                ReservationStatus.WAITING,
+                priority,
+                null);
+    }
+
+    // 상태 변경 메서드
+
+    public void expire() {
+        this.reservationStatus = ReservationStatus.EXPIRED;
+    }
+
+    public void available() {
+        this.reservationStatus = ReservationStatus.AVAILABLE;
+    }
+
+    public void complete(){
+        this.reservationStatus = ReservationStatus.COMPLETED;
+    }
+
+    public void setRentTimeout(LocalDateTime rentTimeout) {this.rentTimeout = rentTimeout;}
+
+    public void updatePriority(String priority) {
+        this.priority = priority;
+    }
 }
