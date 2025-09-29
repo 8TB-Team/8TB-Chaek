549d6e2ac4d4bd70354d7e53ed6b0380bef72781
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-09-29 10:09:46 +0900

    feat: rental @EntityGraph 추가

--- src/main/java/com/example/chackchack/domain/rental/repository/RentalRepository.java
+++ src/main/java/com/example/chackchack/domain/rental/repository/RentalRepository.java
@@ -5,15 +5,21 @@ import com.example.chackchack.domain.rental.entity.Rental;
 import com.example.chackchack.domain.rental.enums.RentalStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+    @EntityGraph(attributePaths = {"bookItem", "user"})
     List<Rental> findAllByStatusAndDueDateBefore(RentalStatus status, LocalDate date);
 
+    @EntityGraph(attributePaths = {"bookItem", "user"})
     Page<Rental> findByUserId(Long userId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"bookItem", "user"})
+    Page<Rental> findAll(Pageable pageable);
 
     boolean existsByBookItemAndStatus(BookItem bookItem, RentalStatus status);
 }
