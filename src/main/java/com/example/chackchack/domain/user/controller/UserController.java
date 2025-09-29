ee7b9cf43ec5610e236a2ac47eabce1d08fcb2b3
OverNight <hidden98716@gmail.com>
2025-09-29 11:24:38 +0900

    refactor: api 링크 개선

--- src/main/java/com/example/chackchack/domain/user/controller/UserController.java
+++ src/main/java/com/example/chackchack/domain/user/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserExternalService userExternalService;
