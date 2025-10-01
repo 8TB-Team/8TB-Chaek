3f8eb40b33e6759dcc5a077f11a479fa28ce070e
LeeDongJae-KR <movementj98@gmail.com>
2025-10-01 14:22:30 +0900

    feat: Reservation Index 적용

--- src/main/java/com/example/chackchack/domain/reservation/entity/Reservation.java
+++ src/main/java/com/example/chackchack/domain/reservation/entity/Reservation.java
@@ -12,7 +12,27 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Table(name = "reservation")
+@Table(name = "reservation", indexes = {
+        // 사용자 예약 목록 조회용
+        @Index(name = "idx_user_id_created_at",
+                columnList = "user_id, created_at DESC"),
+
+        // 도서별 대기열 조회
+        @Index(name = "idx_book_item_status_priority",
+                columnList = "book_item_id,reservation_status, priority ASC"),
+
+        //만료된 예약 조회
+        @Index(name = "idx_status_rent_timeout",
+                columnList = "reservation_status, rent_time_out"),
+
+        // 상태별 예약 목록 조회
+        @Index(name = "idx_status_created_at",
+                columnList = "reservation_status, created_at_DESC"),
+
+        //예약 존재 여부
+        @Index(name = "idx_book_item_status",
+        columnList = "book_item_id, reservation_status")
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Reservation extends BaseEntity {
 
@@ -38,7 +58,7 @@ public class Reservation extends BaseEntity {
     @Column(name = "rent_timeout")
     private LocalDateTime rentTimeout;
 
-    private Reservation(User user, BookItem bookItem, ReservationStatus reservationStatus,Integer priority, LocalDateTime rentTimeout) {
+    private Reservation(User user, BookItem bookItem, ReservationStatus reservationStatus, Integer priority, LocalDateTime rentTimeout) {
         this.user = user;
         this.bookItem = bookItem;
         this.reservationStatus = reservationStatus;
@@ -47,7 +67,7 @@ public class Reservation extends BaseEntity {
 
     }
 
-    public static Reservation of (User user, BookItem bookItem, Integer priority) {
+    public static Reservation of(User user, BookItem bookItem, Integer priority) {
         return new Reservation(
                 user,
                 bookItem,
@@ -66,11 +86,13 @@ public class Reservation extends BaseEntity {
         this.reservationStatus = ReservationStatus.AVAILABLE;
     }
 
-    public void complete(){
+    public void complete() {
         this.reservationStatus = ReservationStatus.COMPLETED;
     }
 
-    public void setRentTimeout(LocalDateTime rentTimeout) {this.rentTimeout = rentTimeout;}
+    public void setRentTimeout(LocalDateTime rentTimeout) {
+        this.rentTimeout = rentTimeout;
+    }
 
     public void updatePriority(Integer priority) {
         this.priority = priority;
