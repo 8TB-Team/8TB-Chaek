e999c56a11fdd1f5d2a684e2c9d358a57e6d76ad
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-01 14:16:52 +0900

    feat: 이벤트 생성, 유저 이벤트 등록 API 적용

--- src/main/java/com/example/chackchack/domain/flashEvent/controller/FlashEventController.java
+++ src/main/java/com/example/chackchack/domain/flashEvent/controller/FlashEventController.java
@@ -1,14 +1,26 @@
 package com.example.chackchack.domain.flashEvent.controller;
 
+import com.example.chackchack.domain.common.dto.AuthUser;
+import com.example.chackchack.domain.flashEvent.dto.request.EventCreateRequest;
 import com.example.chackchack.domain.flashEvent.service.FlashEventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api/event")
 @RequiredArgsConstructor
 public class FlashEventController {
 
     private final FlashEventService flashEventService;
 
+    @PostMapping
+    public void createEvent(@RequestBody EventCreateRequest request) {
+        flashEventService.createNewEvent(request);
+    }
 
+    @GetMapping("/{eventId}")
+    public void registerToEvent(@PathVariable Long eventId, @AuthenticationPrincipal AuthUser authUser) {
+        flashEventService.registerUserToEvent(eventId, authUser.getId());
+    }
 }
