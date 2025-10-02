709dfddaa164393d7204314ec2a1bade9ae4fe8e
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-10-02 14:05:48 +0900

    refactor: rental 반납 rentalId-> serial Number

---
+++ src/main/java/com/example/chackchack/domain/rental/dto/request/RentalUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.example.chackchack.domain.rental.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RentalUpdateRequest {
+    @NotBlank(message = "Serial Number를 입력해 주세요.")
+    private String serialNumber;
+}
